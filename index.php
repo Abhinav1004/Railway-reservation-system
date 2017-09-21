a31188dc68c70bb7f72cc955354155c37bade90c
Abhinav Kumar Jha <abhinavkrjha10@gmail.com>
2017-09-21 17:41:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include('header.php'); ?>
+			 
+			  <div class="col-md-12 ">
+				<div class="slider">
+					<img src="img/train2" width="100%" />
+				</div>
+			  </div>
+<?php include('footer.php'); ?>
+ 
+
+       
